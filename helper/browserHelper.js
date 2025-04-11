15cd41b649b14f9aad18c42637efcae0f71420b2
Hozefa Ali <111397992+zef-ces@users.noreply.github.com>
2025-04-11 18:23:44 +1000

    maximize the browser, as elements are not coming in the viewport even through scrollToView was added

--- helper/browserHelper.js
+++ helper/browserHelper.js
@@ -5,6 +5,8 @@ async function startBrowser() {
     // Ensure ChromeDriver path is used
     process.env.PATH += `:${chromedriver.path}`;
     let driver = await new Builder().forBrowser('chrome').build();
+    await driver.manage().window().maximize();
+
     // Set implicit wait for 60 seconds
     await driver.manage().setTimeouts({ implicit: 60000 });
     return driver;
